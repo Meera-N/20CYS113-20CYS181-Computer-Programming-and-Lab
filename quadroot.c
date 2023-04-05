bad153086499b54d692706a1b8e44c3fc0d4127f
Meera-N <116825535+Meera-N@users.noreply.github.com>
2023-04-05 09:44:43 +0530

    (05-04-23) Practice
    Finding root of quadratic equation

---
+++ quadroot.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <math.h>
+
+int main () {
+
+    //declaring variables	
+    float a, b, c, rt ;
+    //getting input
+    printf("In the equation ax^2 + bx + c,\nWe need the values of a, b and c.");
+    printf("\nEnter value of a :\n");
+    scanf("%f", &a);
+    printf("\nEnter value of b :\n");
+    scanf("%f", &b);
+    printf("\nEnter value of c :\n");
+    scanf("%f", &c);
+    //calculation
+    rt = (-b + sqrt(b*b - 4*a*c))/2*a;
+    //output
+    printf("The root is %lf",rt);
+}
