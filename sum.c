540383b86839fc8740b1ace76580eb981d10e39d
Meera-N <116825535+Meera-N@users.noreply.github.com>
2023-04-10 09:13:48 +0530

    Sum of n numbers

---
+++ sum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main () {
+
+//declaring variables
+
+int sum, num ;
+
+//getting input
+
+printf("Please enter N of which sum should be found: \n");
+
+scanf("%d",&num);
+
+//calculation 
+
+sum = num * ((num + 1))/ 2 ;
+
+//outputting
+
+printf("The sum of first %d numbers is %d \n",num, sum);
+
+}
+
