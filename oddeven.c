91640a14e2d133ced39ee3bf961a67e66ae06b22
Meera-N <116825535+Meera-N@users.noreply.github.com>
2023-04-10 09:14:18 +0530

    Odd or even checker

---
+++ oddeven.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+        //declaring 
+	int num ;
+
+	//getting input
+	printf("Please enter the number you want to check if odd or even \n");
+
+	scanf("%d", &num);
+
+
+	//condition and result
+	
+        if (num % 2 != 0){
+
+		printf("The number is odd \n");
+        }
+	else{
+
+		printf("The number is even \n");
+	}}
+
